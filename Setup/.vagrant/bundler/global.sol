588c38f3b2e46109e90a10cea3f5b610e25e5340
Win Paing Soe <winpaingsoe49@gmail.com>
2024-09-11 02:29:18 +0700

    modify VagrantFile

---
+++ Setup/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["dotenv-deployment",["~> 0.0.2"]],["dotenv",["~> 0.9"]],["vagrant-env",["= 0.0.3"]]],"checksum":"3389f64107f5d40b880499ae6da4a525cba3d54b872fb5ed107cf51209cfa691","vagrant_version":"2.4.1"}
